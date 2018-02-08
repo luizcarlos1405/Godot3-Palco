86b71c9f0e041f6fe107e62421aadd27932e1b1a
Luiz <luizcarlos1405@gmail.com>
2018-02-08 07:59:31 -0300

    pequenas correções

--- atual/personagens/Personagem.gd
+++ atual/personagens/Personagem.gd
@@ -6,7 +6,7 @@ export var aceleracao = 3000
 export var desaceleracao = 3000
 export var aceleracao_gravidade = 1500
 export var velocidade_queda = 600
-export var fator_forca_pulo = 2
+export var fator_pulo_fraco = 2
 
 var direcao_entrada = Vector2()
 var direcao_ultimo_movimento = Vector2(1, 0)
@@ -33,30 +33,8 @@ func _physics_process(delta):
 	# Função que lida com todo o movimento no eixo x do personagem.
 	_movimento_horizontal(delta)
 	
-	# Se o personagem estivere no chão
-	if is_on_floor():
-		# Zeramos a velocidade em 0
-		velocidade_atual.y = 0
-		# E se ele estiver pressinando o botão para pular e está no chão
-		if pular_pressionado:
-			# Aplicamos a velocidade de pulo
-			velocidade_atual.y = velocidade_pulo
-	
-	# Se o personagem bater a cabeça no teto dua velocidade também é zerada
-	if is_on_ceiling():
-		velocidade_atual.y = 0
-	
-	""" Estas duas linhas aplicam a aceleração da gravidade no personagem
-	e fazem com que ele tenda à velocidade de queda. """
-	var variacao = aceleracao_gravidade * delta
-	""" Se o botão de pular não estiver sendo pressoinado e a velocidade
-	em y estiver negativa (personagem indo pra cima), aplicamos uma
-	gravidade mais forte multiplicando pelo fator de pulo. """
-	if not pular_pressionado and velocidade_atual.y < 0:
-		variacao = variacao * fator_forca_pulo
-	
-	# Finalmente aplica-se a velociade em y
-	velocidade_atual.y = aproximar(velocidade_atual.y, velocidade_queda, variacao)
+	# Função que lida com todo o movimento no eixo y do personagem.
+	_movimento_vertical(delta)
 	
 	""" Função move_and_slide move e desliza o jogador por um body
 	caso seja necessário. Esta função também multiplica o argumento
@@ -97,6 +75,33 @@ func _movimento_horizontal(delta):
 	da velocidade_atual.x para a velocidade_alvo com a variação calculada
 	acima (ou seja, numa taxa de aceleracao ou desaceleracao por segundo). """
 	velocidade_atual.x = aproximar(velocidade_atual.x, velocidade_alvo * direcao_entrada.x, variacao)
+
+func _movimento_vertical(delta):
+	# Se o personagem estiver no chão
+	if is_on_floor():
+		# Zeramos a velocidade em 0
+		velocidade_atual.y = 0
+		# E se ele estiver pressionando o botão para pular e está no chão
+		if pular_pressionado:
+			# Aplicamos a velocidade de pulo
+			velocidade_atual.y = velocidade_pulo
+	
+	# Se o personagem bater a cabeça no teto sua velocidade em y também é zerada
+	if is_on_ceiling():
+		velocidade_atual.y = 0
+	
+	""" Calculamos a variação da velocidade em y levando em conta a aceleracao
+	da gravidade no jogo. """
+	var variacao = aceleracao_gravidade * delta
+	""" Se o botão de pular não estiver sendo pressionado e a velocidade
+	em y estiver negativa (personagem indo pra cima), aplicamos uma
+	gravidade mais forte multiplicando pelo fator de pulo fraco que dita a
+	diferença de segurar o botão de pulo ou soltar rapidamente. """
+	if not pular_pressionado and velocidade_atual.y < 0:
+		variacao = variacao * fator_pulo_fraco
+	
+	# Finalmente aplica-se a velociade em y
+	velocidade_atual.y = aproximar(velocidade_atual.y, velocidade_queda, variacao)
 
 """ Essa função aproxima o valor atual para o final em uma taxa linear
 de variacao por chamada da função. Um pouco complicado de entender mesmo.
